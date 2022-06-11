181a33264a553e0b4eab6ef04a8cf3e8e006178f
Jay Arredondo <jearredondo13@gmail.com>
2022-06-11 11:04:03 -0500

    before implemenation of Pinia

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ const userData = reactive({
   username: 'jayman123'
 })
 
+// provides data to descendants of App.vue
 provide('userData', userData);
 
 </script>
--- src/stores/counter.js
+++ src/stores/counter.js
@@ -2,12 +2,15 @@ import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore({
   id: 'counter',
+  // holds all data properties
   state: () => ({
     counter: 0
   }),
+  // methods that can grab data from the state and change it.
   getters: {
     doubleCount: (state) => state.counter * 2
   },
+  // store methods which can access data in the state
   actions: {
     increment() {
       this.counter++
